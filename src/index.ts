7b2dba621d40ce7dfd9d7a959b19f65608960be6
Fredrik Zeiner <Privat@Fredriks-MacBook-Pro.local>
2020-12-28 23:34:29 +0100

    Added starting instructions

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { Parser } from './parser/parser';
 import { BinOpNode } from './types';
 import { Visitor } from './visitor/visitor';
 
+console.info('Write expression, click "enter" to evaluate');
+
 process.stdin.on('data', (data) => {
   const parser = new Parser(new Lexer(data.toString()));
   const res = new Visitor().walk(parser.parse() as BinOpNode);
