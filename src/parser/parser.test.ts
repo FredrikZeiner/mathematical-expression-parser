cf523f523fa017c632c030aeb750e60f7450492a
Fredrik Zeiner <Privat@Fredriks-MacBook-Pro.local>
2020-12-29 17:55:51 +0100

    Renamed expressions to expression.

--- src/parser/parser.test.ts
+++ src/parser/parser.test.ts
@@ -6,36 +6,36 @@ import { ast1, ast2, ast3, ast4, ast6 } from '../../data/asts';
 
 describe('class Parser', () => {
   it('should produce correct AST for term', () => {
-    const query = '1 + 2 + 3';
-    const parser = new Parser(new Lexer(query));
+    const expression = '1 + 2 + 3';
+    const parser = new Parser(new Lexer(expression));
     const tree = parser.parse();
     expect(tree).to.deep.equal(ast1);
   });
 
   it('should produce correct AST for expr and term', () => {
-    const query = '1 * 2 + 3';
-    const parser = new Parser(new Lexer(query));
+    const expression = '1 * 2 + 3';
+    const parser = new Parser(new Lexer(expression));
     const tree = parser.parse();
     expect(tree).to.deep.equal(ast2);
   });
 
   it('should produce correct AST for expr and term', () => {
-    const query = '2 * 2 + 3 + 4';
-    const parser = new Parser(new Lexer(query));
+    const expression = '2 * 2 + 3 + 4';
+    const parser = new Parser(new Lexer(expression));
     const tree = parser.parse();
     expect(tree).to.deep.equal(ast3);
   });
 
   it('should produce correct AST for expr and term', () => {
-    const query = '1 * 2 + 3 * 4';
-    const parser = new Parser(new Lexer(query));
+    const expression = '1 * 2 + 3 * 4';
+    const parser = new Parser(new Lexer(expression));
     const tree = parser.parse();
     expect(tree).to.deep.equal(ast4);
   });
 
   it('should produce correct AST for expr and term', () => {
-    const query = '10 / 2 * 3';
-    const parser = new Parser(new Lexer(query));
+    const expression = '10 / 2 * 3';
+    const parser = new Parser(new Lexer(expression));
     const tree = parser.parse();
     expect(tree).to.deep.equal(ast6);
   });
